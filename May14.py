3e43a8829e753523e15b22531b72cb75e3e2068c
RamandeepSinghMakkar <126968397+RamandeepSinghMakkar@users.noreply.github.com>
2024-06-14 13:08:42 +0530

    Create May14.py
    945. . Minimum Increment to Make Array Unique

---
+++ May14.py
@@ -0,0 +1,17 @@
+#Time complexity: O(sort)
+#Space complexity: O(sort)
+#Leetcode Question Link: https://leetcode.com/problems/minimum-increment-to-make-array-unique/description/?envType=daily-question&envId=2024-06-14
+
+#945. Minimum Increment to Make Array Unique
+#You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1. Return the minimum number of moves to make every value in nums unique. The test cases are generated so that the answer fits in a 32-bit integer.
+
+class Solution:
+  def minIncrementForUnique(self, nums: List[int]) -> int:
+    ans = 0
+    minAvailable = 0
+
+    for num in sorted(nums):
+      ans += max(minAvailable - num, 0)
+      minAvailable = max(minAvailable, num) + 1
+
+    return ans
