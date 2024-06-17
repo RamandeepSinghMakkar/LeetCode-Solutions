cd690fa4441f06523bb48f3475d2a50d9a34adcd
RamandeepSinghMakkar <126968397+RamandeepSinghMakkar@users.noreply.github.com>
2024-06-17 12:51:07 +0530

    Added Question Link
    502. IPO (Hard)

--- 15MAY.cpp
+++ 15MAY.cpp
@@ -1,8 +1,9 @@
 #Time complexity: O(nlogn)
 #Space complexity: O(n)
 
-#502. IPO (Hard)
+# LeetCode Question Link: https://leetcode.com/problems/ipo/description/
 
+#502. IPO (Hard)
 #Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects. 
 #You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
 #Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
