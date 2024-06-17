0dc23f6491233c5a36cd32997b54496f9651786f
RamandeepSinghMakkar <126968397+RamandeepSinghMakkar@users.noreply.github.com>
2024-06-17 12:49:13 +0530

    Update 16May.py
    330. Patching Array (Hard)

--- 16May.py
+++ 16May.py
@@ -1,9 +1,9 @@
 # Time complexity: O(n)
 #Space complexity: O(1)
+
 #Leetcode Question Link: https://leetcode.com/problems/patching-array/description/?envType=daily-question&envId=2024-06-16
 
 # 330. Patching Array (Hard)
-
 #Given a sorted integer array nums and an integer n, add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
 #Return the minimum number of patches required.
 
