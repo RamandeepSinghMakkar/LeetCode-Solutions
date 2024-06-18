17965deb3732ac351906cfafd3498470b51d1200
RamandeepSinghMakkar <126968397+RamandeepSinghMakkar@users.noreply.github.com>
2024-06-18 13:27:23 +0530

    Update 14June.py
    945. Minimum Increment to Make Array Unique (Medium)

--- 14June.py
+++ 14June.py
@@ -2,7 +2,7 @@
 #Space complexity: O(sort)
 #Leetcode Question Link: https://leetcode.com/problems/minimum-increment-to-make-array-unique/description/?envType=daily-question&envId=2024-06-14
 
-#945. Minimum Increment to Make Array Unique
+#945. Minimum Increment to Make Array Unique (Medium)
 #You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1. Return the minimum number of moves to make every value in nums unique. The test cases are generated so that the answer fits in a 32-bit integer.
 
 class Solution:
